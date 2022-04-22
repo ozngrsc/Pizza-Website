302d1b41dbe12496a6bbe7d38b89dae1693ac8cc
ozngrsc <ozangursucu@hotmail.com>
2022-04-22 14:51:43 +0300

    heroElements

--- src/components/Hero/HeroElements.js
+++ src/components/Hero/HeroElements.js
@@ -8,3 +8,57 @@ export const HeroContainer = styled.div`
   background-position: center;
   background-size: cover;
 `;
+
+export const HeroContent = styled.div`
+  height: calc(100vh, -80px);
+  max-height: 100%;
+  width: 100vw;
+  padding: 0rem calc((100vw - 1300px) / 2);
+`;
+
+export const HeroItems = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  height: 100vh;
+  max-height: 100%;
+  padding: 0 2rem;
+  width: 650px;
+  color: #fff;
+  text-transform: uppercase;
+  line-height: 1;
+  font-weight: bold;
+
+  @media screen and (max-width: 650px) {
+    width: 100%;
+  }
+`;
+
+export const HeroH1 = styled.div`
+  font-size: clamp(2.5rem, 10vw, 5rem);
+  margin-bottom: 1rem;
+  box-shadow: 3px 5px #e9ba23;
+  letter-spacing: 3px;
+`;
+
+export const HeroP = styled.p`
+  font-size: clamp(2rem, 2.5vw, 3rem);
+  margin-bottom: 2rem;
+`;
+
+export const HeroBtn = styled.div`
+  font-size: 1.4rem;
+  padding: 1rem 4rem;
+  border: none;
+  background: #e31837;
+  color: #fff;
+  transition: 0.2s ease-out;
+
+  &:hover {
+    background: #ffc500;
+    transition: 0.2s ease-out;
+    cursor: pointer;
+    color: #000;
+  }
+`;
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import Navbar from "../Navbar";
-import { HeroContainer } from "./HeroElements";
+import {
+  HeroContainer,
+  HeroContent,
+  HeroItems,
+  HeroH1,
+  HeroP,
+  HeroBtn,
+} from "./HeroElements";
 
 const Hero = () => {
   return (
     <HeroContainer>
       <Navbar />
+      <HeroContent>
+        <HeroItems>
+          <HeroH1>Greatest Pizza Ever</HeroH1>
+          <HeroP>Ready in 60 seconds</HeroP>
+          <HeroBtn>Place Order</HeroBtn>
+        </HeroItems>
+      </HeroContent>
     </HeroContainer>
   );
 };
