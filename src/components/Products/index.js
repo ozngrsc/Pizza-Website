dc4ae862990e24ebf8eab8d9230171a06e55711f
ozngrsc <ozangursucu@hotmail.com>
2022-04-25 12:44:39 +0300

    product2

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,21 +1,36 @@
 import React from "react";
+import {
+  ProductsContainer,
+  ProductsHeading,
+  ProductWrapper,
+  ProductCard,
+  ProductImg,
+  ProductInfo,
+  ProductTitle,
+  ProductDesc,
+  ProductPrice,
+  ProductButton,
+} from "./ProductsElements";
 
 const Products = () => {
   return (
     <ProductsContainer>
       <ProductsHeading>heading</ProductsHeading>
-      <ProductsWrapper>
+      <ProductWrapper>
         {data.map((product, index) => {
           return (
             <ProductCard key={index}>
               <ProductImg src={product.img} alt={product.alt} />
               <ProductInfo>
                 <ProductTitle>{product.name}</ProductTitle>
+                <ProductDesc>{product.desc}</ProductDesc>
+                <ProductPrice>{product.price}</ProductPrice>
+                <ProductButton>{product.button}</ProductButton>
               </ProductInfo>
             </ProductCard>
           );
         })}
-      </ProductsWrapper>
+      </ProductWrapper>
     </ProductsContainer>
   );
 };
