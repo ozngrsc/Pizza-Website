66c88c25816f46f5db7505e878deb446395f32bf
ozngrsc <ozangursucu@hotmail.com>
2022-04-25 15:16:53 +0300

    deserts

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Hero from "./components/Hero";
 import Products from "./components/Products";
 import { GlobalStyle } from "./globalStyles";
-import { productData } from "./components/Products/data";
+import { productData, productDataTwo } from "./components/Products/data";
 import Feature from "./components/Feature";
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
       <Hero />
       <Products heading="Choose your favorite" data={productData} />
       <Feature />
+      <Products heading="Sweet Treats for You" data={productDataTwo} />
     </Router>
   );
 }
--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -1,6 +1,9 @@
 import product1 from "../../images/product-1.jpg";
 import product2 from "../../images/product-2.jpg";
 import product3 from "../../images/product-3.jpg";
+import sweet1 from "../../images/sweet3.jpg";
+import sweet2 from "../../images/sweet-2.jpg";
+import sweet3 from "../../images/sweet-3.jpg";
 
 export const productData = [
   {
@@ -25,6 +28,33 @@ export const productData = [
     name: "Veggie Pizza",
     desc: "Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto",
     price: "$14.99",
+    button: "Add to Cart",
+  },
+];
+
+export const productDataTwo = [
+  {
+    img: sweet2,
+    alt: "Donuts",
+    name: "Doughlicious",
+    desc: "Belgian chocolate glazed donuts covered in icing with sprinkles on top",
+    price: "$9.99",
+    button: "Add to Cart",
+  },
+  {
+    img: sweet3,
+    alt: "Ice Cream",
+    name: "Caramel Wonder",
+    desc: "Vanilla ice cream covered with caramel and chocolate glaze topped with a coco stick",
+    price: "$12.99",
+    button: "Add to Cart",
+  },
+  {
+    img: sweet1,
+    alt: "Brownie",
+    name: "Brownie Bunch",
+    desc: "Double fudge brownie squares topped with white chocolate pieces and macadamia nuts",
+    price: "$9.99",
     button: "Add to Cart",
   },
 ];
