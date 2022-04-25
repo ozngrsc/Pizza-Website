14df58132b1f68499489137b17042ffe4ade984e
ozngrsc <ozangursucu@hotmail.com>
2022-04-25 16:27:06 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Products from "./components/Products";
 import { GlobalStyle } from "./globalStyles";
 import { productData, productDataTwo } from "./components/Products/data";
 import Feature from "./components/Feature";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Products heading="Choose your favorite" data={productData} />
       <Feature />
       <Products heading="Sweet Treats for You" data={productDataTwo} />
+      <Footer />
     </Router>
   );
 }
